adb2ded7268ebbf5ddced4f15d0b08c032a49ccf
rbgwozdz <richard.gwozdz@gmail.com>
2016-11-08 23:59:18 -0500

    Add project files.

---
+++ GigHubb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GigHubb.MvcApplication" Language="C#" %>
---
+++ GigHubb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigHubb.Startup))]
+namespace GigHubb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
